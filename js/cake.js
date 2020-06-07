b9f45ca77babe97536c707156e617d82a27c75b7
unknown <gauthamsrinivas95@gmail.com>
2020-06-08 04:09:31 +0530

    changes2

--- js/cake.js
+++ js/cake.js
@@ -16,7 +16,7 @@ $(function () {
             timer.hide().delay(750);
             count.hide().delay(750);
             wish.hide().html("<h3>Happy birthday Jiggy!!!</h3>").delay(750).fadeIn(300);
-            txt.hide().html("<h3 id='wishtrue'>Your wish will come true...</h3>").delay(750).fadeIn(300);
+            txt.hide().html("<h3 id='wishtrue'>Your wish will come true...</h3>").delay(900).fadeIn(5000);
             $('#candle').animate({
                 'opacity': '.5'
             }, 100);
